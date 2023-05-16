e4988d142581fb346ff795f7d900066c0f3bdba1
albertoflorence <alberto.sabatier@gmail.com>
2023-05-16 17:10:07 -0300

    requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,10 +1,54 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { searchValidate } from '../validate';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+
+function renderAlbum(props) {
+  const {
+    artistId,
+    artistName,
+    collectionId,
+    collectionName,
+    collectionPrice,
+    artworkUrl100,
+    releaseDate,
+    trackCount,
+  } = props;
+  return (
+    <div key={ collectionId }>
+      <p>{artistId}</p>
+      <p>{artistName}</p>
+      <p>{collectionName}</p>
+      <p>{collectionPrice}</p>
+      <p>{releaseDate}</p>
+      <p>{trackCount}</p>
+      <Link
+        data-testid={ `link-to-album-${collectionId}` }
+        to={ `/album/${collectionId}` }
+      />
+    </div>
+  );
+}
+
+function renderShowcase({ search, albums }) {
+  return (
+    <div>
+      <h1>
+        {'Resultado de álbuns de: '}
+        {search}
+      </h1>
+      {albums.map(renderAlbum)}
+    </div>
+  );
+}
 
 export default function Search() {
   const [search, setSearch] = useState('');
   const [isValid, setIsValid] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [albums, setAlbums] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     setIsValid(searchValidate(search));
@@ -17,19 +61,35 @@ export default function Search() {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
+    const result = await searchAlbumsAPI(search);
+    setLoading(false);
+    if (result.length === 0) setError('Nenhum álbum foi encontrado.');
+    setAlbums(result);
   };
 
   return (
     <section data-testid="page-search">
       <form onSubmit={ handleSubmit }>
-        <input data-testid="search-artist-input" type="text" onChange={ handleChange } />
-        <button
-          data-testid="search-artist-button"
-          type="submit"
-          disabled={ !isValid }
-        >
-          Search
-        </button>
+        {loading ? (
+          'Carregando...'
+        ) : (
+          <div>
+            <input
+              data-testid="search-artist-input"
+              type="text"
+              onChange={ handleChange }
+            />
+            <button
+              data-testid="search-artist-button"
+              type="submit"
+              disabled={ !isValid }
+            >
+              Search
+            </button>
+          </div>
+        )}
+        {error ? <h1>{error}</h1> : renderShowcase({ search, albums })}
       </form>
     </section>
   );
