3158269320d3c5fcbde0d324cfd238e4bcbe6482
albertoflorence <alberto.sabatier@gmail.com>
2023-05-16 16:01:39 -0300

    requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import { getUser } from '../services/userAPI';
 
@@ -14,6 +15,11 @@ export default function Header() {
 
   return (
     <header data-testid="header-component">
+      <nav>
+        <Link data-testid="link-to-search" to="/search">Search</Link>
+        <Link data-testid="link-to-favorites" to="/favorites">Favorites</Link>
+        <Link data-testid="link-to-profile" to="/profile">Profile</Link>
+      </nav>
       <span data-testid="header-user-name">{user.name || 'Carregando...'}</span>
     </header>
   );
