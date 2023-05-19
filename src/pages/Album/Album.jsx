a19cb67bffb3ec6e9d3119b4467cf931b7d9f579
albertoflorence <alberto.sabatier@gmail.com>
2023-05-18 21:12:19 -0300

    add style to Album

--- src/pages/Album/Album.jsx
+++ src/pages/Album/Album.jsx
@@ -1,27 +1,37 @@
 import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
-import getMusics from '../services/musicsAPI';
-import MusicCard from '../components/MusicCard';
+import getMusics from '../../services/musicsAPI';
+import MusicCard from '../../components/MusicCard';
+import './album.css';
 
 export default function Album({ id }) {
   const [info, setInfo] = useState({});
   const [tracks, setTracks] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const handleGetMusic = async () => {
+      setLoading(true);
       const [result, ...results] = await getMusics(id);
+      setLoading(false);
       setInfo(result);
       setTracks(results);
     };
     handleGetMusic();
   }, [id]);
-
+  if (loading) return null;
   return (
-    <section data-testid="page-album">
-      <span data-testid="artist-name">{info.artistName}</span>
-      <span data-testid="album-name">{info.collectionName}</span>
-      <div>
+    <section data-testid="page-album" className="album">
+      <header className="album-header">
+        <img
+          src={ info.artworkUrl100.replace('100x100', '200x200') }
+          alt={ info.collectionName }
+        />
+        <p data-testid="artist-name">{info.artistName}</p>
+        <p data-testid="album-name">{info.collectionName}</p>
+      </header>
+      <div className="album-content">
         {tracks.map(({ trackId, ...props }) => (
           <MusicCard key={ trackId } trackId={ trackId } { ...props } />
         ))}
