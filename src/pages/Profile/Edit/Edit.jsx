e4bdf99f7ffb66a9885257abe5c18f8e170e9ac8
albertoflorence <alberto.sabatier@gmail.com>
2023-05-19 14:27:59 -0300

    add style to profile Edit

--- src/pages/Profile/Edit/Edit.jsx
+++ src/pages/Profile/Edit/Edit.jsx
@@ -1,9 +1,37 @@
+/* eslint-disable react/no-multi-comp */
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { getUser, updateUser } from '../../../services/userAPI';
 import { editUserValidate } from '../../../validate/editUser';
 import './edit.css';
+import Loading from '../../../components/Loading/Loading';
+
+const inputs = [
+  {
+    text: 'Nome',
+    label: 'Fique à vontade para usar seu nome social',
+    placeholder: 'Placeholder',
+    Element: (props) => <input { ...props } />,
+    name: 'name',
+    className: 'edit-name',
+  },
+  {
+    text: 'Email',
+    label: 'Escolha um e-mail que consulta diariamente',
+    placeholder: 'seu_nome@email.com.br',
+    Element: (props) => <input { ...props } />,
+    name: 'email',
+    className: 'edit-email',
+  },
+  {
+    text: 'Descrição',
+    placeholder: 'Sobre mim',
+    Element: (props) => <textarea { ...props } />,
+    name: 'description',
+    className: 'edit-description',
+  },
+];
 
 export default function Edit() {
   const [loading, setLoading] = useState(false);
@@ -30,47 +58,49 @@ export default function Edit() {
     history.push('/profile');
   };
 
-  if (loading) return <p>Carregando...</p>;
-
   return (
     <section data-testid="page-profile-edit" className="edit">
-      <form onSubmit={ handleSubmit }>
-        <input
-          data-testid="edit-input-name"
-          type="text"
-          value={ user.name }
-          name="name"
-          onChange={ handleChange }
-        />
-        <input
-          data-testid="edit-input-email"
-          type="text"
-          value={ user.email }
-          name="email"
-          onChange={ handleChange }
-        />
-        <input
-          data-testid="edit-input-description"
-          type="text"
-          name="description"
-          value={ user.description }
-          onChange={ handleChange }
-        />
-        <input
-          data-testid="edit-input-image"
-          type="text"
-          value={ user.image }
-          name="image"
-          onChange={ handleChange }
-        />
-        <button
-          data-testid="edit-button-save"
-          type="submit"
-          disabled={ !editUserValidate(user) }
-        >
-          Salvar
-        </button>
-      </form>
+      <header className="edit-header">
+        {loading || (
+          <div className="edit-avatar">
+            <img src={ user.image } alt={ user.name } />
+          </div>
+        )}
+      </header>
+      {loading ? <Loading color="secondary" textColor="gray" /> : (
+        <form onSubmit={ handleSubmit } className="edit-content">
+          {inputs.map(({ text, label, placeholder, Element, name, className }) => (
+            <div className="edit-inputGroup" key={ name }>
+              <h3>{text}</h3>
+              {label && <label htmlFor={ name }>{label}</label>}
+              <Element
+                id={ name }
+                type="text"
+                value={ user[name] }
+                name={ name }
+                onChange={ handleChange }
+                placeholder={ placeholder }
+                className={ className }
+              />
+            </div>
+          ))}
+          <input
+            type="text"
+            value={ user.image }
+            name="image"
+            onChange={ handleChange }
+            className="edit-image"
+          />
+          <button
+            data-testid="edit-button-save"
+            type="submit"
+            disabled={ !editUserValidate(user) }
+            className="edit-button"
+          >
+            Salvar
+          </button>
+        </form>
+      )}
     </section>
   );
 }
